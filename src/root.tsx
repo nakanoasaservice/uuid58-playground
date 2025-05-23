c8fe01955e430be56c04dcad98dc96796c29a5f6
Nakano as a Service <nakanoasaservice@gmail.com>
2025-05-23 22:57:32 +0900

    feat: remove service worker

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,5 @@
-import { component$ } from "@builder.io/qwik";
-import { isDev } from "@builder.io/qwik";
-import {
-  QwikCityProvider,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
+import { component$, isDev } from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
@@ -28,7 +23,6 @@ export default component$(() => {
           />
         )}
         <RouterHead />
-        {!isDev && <ServiceWorkerRegister />}
       </head>
       <body lang="en">
         <RouterOutlet />
